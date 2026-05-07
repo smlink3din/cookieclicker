834f6459cc5f1b3215c47fbd437b968486117a84
Sri <smlink3din@gmail.com>
2026-05-07 14:45:04 +0100

    Remove Chrome channel for CI

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],headless:true,channel:'chrome', },
+      use: { ...devices['Desktop Chrome'],headless:true },
     },
 
     {
